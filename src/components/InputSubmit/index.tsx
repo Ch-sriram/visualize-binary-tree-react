a8a9912fce67d979ca1e81a0a6f6fce4250ad1dd
Ch-sriram <sriram.ch1995@gmail.com>
2026-05-18 18:14:08 +0530

    Add a placeholder for `textarea` in the UI

--- src/components/InputSubmit/index.tsx
+++ src/components/InputSubmit/index.tsx
@@ -7,7 +7,7 @@ export interface InputSubmitProps {
 const InputSubmit = React.forwardRef<HTMLTextAreaElement, InputSubmitProps>(({ onClick }, ref) => (
   <div className="input-submit">
     <label htmlFor="numbersInput">Numbers Input</label>
-    <textarea id="number-textarea" ref={ref} />
+    <textarea id="number-textarea" placeholder="1, 2, 3, 4, 5 (comma-separated numbers)" ref={ref} />
     <button className="submit-button" onClick={onClick}>Submit</button>
   </div>
 ));
